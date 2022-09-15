eb0e3ebb9690e07863ea14793f24021c39b3726d
Juan Carlos <jc711646@gmail.com>
2022-09-15 00:28:35 -0500

    Creando componente dinámico para los input

---
+++ src/components/input/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css";
+
+function Input({
+  type = "text",
+  placeholder = "",
+  autofocus = true,
+  autocomplete = "true",
+  required = true,
+  label = "",
+}) {
+  return (
+    <div className="container-field">
+      <label className="label">{label}</label>
+      <input
+        type={type}
+        name=""
+        id=""
+        placeholder={placeholder}
+        className="input"
+        autoFocus={autofocus}
+        autoComplete={autocomplete}
+        required={required}
+      />
+    </div>
+  );
+}
+
+export default Input;
