f5a887b3a225d14d13df00088ea6f37dee5d7b7c
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-13 00:06:25 -0500

    Agregar rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import Login from './views/login';
-import './App.css'
+import Login from "./views/login";
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+const token = window.localStorage.token;
+
+export const Home = () => <h1>Estas en el home</h1>;
+export const Register = () => <h1>Estas en el register</h1>;
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {token ? (
+        <Routes>
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      )}
     </div>
   );
 }
