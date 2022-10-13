ecdf9e0efa1ce5139c824afdfbb44b66624d7e5b
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-13 00:06:58 -0500

    Actualizar componente de Links para los Links de react router

--- src/components/links/index.js
+++ src/components/links/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
-function Links({text = "", href = "#"}) {
+function Links({ text = "", href = "#", isLink = false }) {
+  if (isLink) {
+    return (
+      <Link to = {href} className="link">
+        {text}
+      </Link>
+    );
+  }
   return (
     <a href={href} className="link">
       {text}
