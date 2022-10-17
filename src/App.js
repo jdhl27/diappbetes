76faf05631965c509f7ef1b5d6dddd788f9479c7
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-17 18:45:36 -0500

    Actualizar rutas

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Register from "./views/register";
 
-const token = window.localStorage.token;
-
-export const Home = () => <h1>Estas en el home</h1>;
+export const Home = () => {
+  const token = window.localStorage.token;
+  if (token) {
+    return <h1>Estas en el home</h1>;
+  }
+  return <Navigate to="/login" replace={true} />;
+};
 
 function App() {
+  const token = window.localStorage.token;
   return (
     <div className="App">
-      {token ? (
-        <Routes>
-          <Route path="*" element={<Navigate to="/" replace />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route path="*" element={<Navigate to="/" replace />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      )}
+      <Routes>
+        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/" element={token ? <Home /> : <Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/login"
+          element={token ? <Navigate to="/" replace /> : <Login />}
+        />
+        <Route path="/register" element={token ? <Navigate to="/" replace /> : <Register />} />
+      </Routes>
     </div>
   );
 }
