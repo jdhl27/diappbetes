d7211b78bb84cc11814275fdfbf276ff7d574f1a
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-17 18:43:50 -0500

    Modificar el componente de boton

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./styles.css";
 
-function ButtonComponent({text = "", href = "#"}) {
+function ButtonComponent({ text = "", href = "#", onClick = null }) {
+  if (onClick) {
+    return (
+      <div className="button" onClick={() => onClick()}>
+        {text}
+      </div>
+    );
+  }
   return (
     <a href={href} className="button">
       {text}
