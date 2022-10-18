56c8a358e7a081616396dffb5ec175c02993cb2e
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-18 02:06:58 -0500

    Agregando endpoint para info user e instalando material UI

--- src/API/endpoints/user.js
+++ src/API/endpoints/user.js
@@ -12,6 +12,10 @@ class User {
   GetUsers = (params = {}) => {
     return new API("users/").get(params);
   };
+
+  GetUser = (params = {}) => {
+    return new API("user/").get(params);
+  };
 }
 
 export default new User();
