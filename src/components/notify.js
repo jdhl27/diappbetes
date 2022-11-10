eb865321101447fb14708e9cea26d71120043029
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-11-10 01:16:27 -0500

    Crear componente de notificaciones

---
+++ src/components/notify.js
@@ -0,0 +1,35 @@
+import { toast } from "react-toastify";
+
+export const notify = (
+    msg = "",
+    type = "warn",
+    position = toast.POSITION.TOP_LEFT
+  ) => {
+    switch (type) {
+      case "info":
+        toast.info(msg || "Info Notification !", {
+          position,
+        });
+        break;
+      case "success":
+        toast.success(msg || "Success Notification !", {
+          position,
+        });
+        break;
+      case "error":
+        toast.error(msg || "Error Notification !", {
+          position,
+        });
+        break;
+      case "warn":
+        toast.warn(msg || "Warning Notification !", {
+          position,
+        });
+        break;
+
+      default:
+        toast.info(msg || "Info Notification !", {
+          position,
+        });
+    }
+  };
