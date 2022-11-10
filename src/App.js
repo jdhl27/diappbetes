61c6d1f5b1ad065970238121cddc06f5c782ca36
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-11-10 01:17:16 -0500

    Actualizando App

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Register from "./views/register";
 import Dashboard from "./views/dashboard";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
+import { CssBaseline } from "@mui/material";
+import UserState from "../src/contexts/user/userState";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const token = window.localStorage.token;
   return (
-    <div className="App">
-      <Routes>
-        <Route path="*" element={<Navigate to="/" replace />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route
-          path="/login"
-          element={token ? <Navigate to="/" replace /> : <Login />}
-        />
-        <Route
-          path="/register"
-          element={token ? <Navigate to="/" replace /> : <Register />}
-        />
-      </Routes>
-    </div>
+    <UserState>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <ToastContainer />
+          <Routes>
+            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route
+              path="/login"
+              element={token ? <Navigate to="/" replace /> : <Login />}
+            />
+            <Route
+              path="/register"
+              element={token ? <Navigate to="/" replace /> : <Register />}
+            />
+          </Routes>
+        </div>
+      </ThemeProvider>
+    </UserState>
   );
 }
 
