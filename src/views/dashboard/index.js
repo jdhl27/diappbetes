f50776aa6932841c16be70a4344dc6650450c7e7
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-11-12 13:28:12 -0500

    Código limpio en dashboard view

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,53 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
-import User from "../../API/endpoints/user";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 import { DashboardLayout } from "../../components/dashboard/dashboard-layout";
-import Header from "../../components/header";
 import Loading from "../../components/loading";
 
 const Dashboard = () => {
   const [loading, setLoading] = useState(true);
-  const [dataUser, setUser] = useState({});
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = window.localStorage.token;
-    if (token) {
-      User.GetUser()
-        .then((response) => {
-          if (response.status >= 200 && response.status < 300) {
-            setUser(response?.data?.user);
-          }
-          setLoading(false);
-        })
-        .catch((err) => {
-          setLoading(false);
-          console.log("error: ", err);
-        });
-    }
-  }, []);
-
-  const handleLogout = async () => {
-    setLoading(true);
-    await localStorage.removeItem("token");
-    setTimeout(() => {
-      navigate("/");
-      setLoading(false);
-    }, 1000);
-  };
 
   const token = window.localStorage.token;
   if (token) {
     return (
       <DashboardLayout>
-        {loading && <Loading />}
-        {/* <Header
-          userData={dataUser}
-          onLogout={() => {
-            handleLogout();
-          }}
-        /> */}
+        {/* {loading && <Loading />} */}
+        <h1>Estoy en la vista dashboard</h1>
       </DashboardLayout>
     );
   }
