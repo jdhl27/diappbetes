a59e49369e41eddb47f60efc6b6b64805589054b
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-11-13 14:11:20 -0500

    Agregar la vista mi cuenta

---
+++ src/components/account/account-profile-details.js
@@ -0,0 +1,157 @@
+import { useContext, useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  Grid,
+  TextField,
+} from "@mui/material";
+import UserContext from "../../contexts/user/userContext";
+import User from "../../API/endpoints/user";
+
+const states = [
+  {
+    value: "colombia",
+    label: "Colombia",
+  },
+  {
+    value: "argentina",
+    label: "Argentina",
+  },
+  {
+    value: "peru",
+    label: "Perú",
+  },
+];
+
+export const AccountProfileDetails = (props) => {
+  // Context for user selected
+  const { user, updateUser } = useContext(UserContext);
+
+  useEffect(() => {
+    if (Object.keys(user).length === 0) {
+      const token = window.localStorage.token;
+      if (token) {
+        User.GetUser()
+          .then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+              updateUser(response?.data?.user);
+            }
+          })
+          .catch((err) => {
+            console.log("error: ", err);
+          });
+      }
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    updateUser({
+      ...user,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <form autoComplete="off" noValidate {...props}>
+      <Card>
+        <CardHeader subheader="Puedes editar la información" title="Mi perfil" />
+        <Divider />
+        <CardContent>
+          <Grid container spacing={3}>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                helperText="Escribe tu nombre"
+                label="Nombre"
+                name="firstName"
+                onChange={handleChange}
+                required
+                value={user.firstName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Apellido"
+                name="lastName"
+                onChange={handleChange}
+                required
+                value={user.lastName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Correo electrónico"
+                name="email"
+                onChange={handleChange}
+                required
+                value={user.email}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Número de celular"
+                name="phone"
+                onChange={handleChange}
+                type="number"
+                value={user.phone}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Ciudad"
+                name="ciudad"
+                onChange={handleChange}
+                required
+                value={user.country}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="País"
+                name="pais"
+                onChange={handleChange}
+                required
+                select
+                SelectProps={{ native: true }}
+                value={user.state}
+                variant="outlined"
+              >
+                {states.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </TextField>
+            </Grid>
+          </Grid>
+        </CardContent>
+        <Divider />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            p: 2,
+          }}
+        >
+          <Button color="primary" variant="contained">
+            Guardar cambios
+          </Button>
+        </Box>
+      </Card>
+    </form>
+  );
+};
---
+++ src/components/account/account-profile.js
@@ -0,0 +1,76 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  Typography,
+} from "@mui/material";
+import { useContext, useEffect } from "react";
+import UserContext from "../../contexts/user/userContext";
+import User from "../../API/endpoints/user";
+
+export const AccountProfile = (props) => {
+  // Context for user selected
+  const { user, updateUser } = useContext(UserContext);
+
+  useEffect(() => {
+    if (Object.keys(user).length === 0) {
+      const token = window.localStorage.token;
+      if (token) {
+        User.GetUser()
+          .then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+              updateUser(response?.data?.user);
+            }
+          })
+          .catch((err) => {
+            console.log("error: ", err);
+          });
+      }
+    }
+  }, []);
+
+  return (
+    <Card {...props}>
+      <CardContent>
+        <Box
+          sx={{
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Avatar
+            src={user.avatar}
+            sx={{
+              height: 64,
+              mb: 2,
+              width: 64,
+            }}
+          />
+          <Typography color="textPrimary" gutterBottom variant="h5">
+            {user.displayName}
+          </Typography>
+          {user?.city && (
+            <Typography color="textSecondary" variant="body2">
+              {`${user?.city} ${user?.country}`}
+            </Typography>
+          )}
+
+          <Typography color="textSecondary" variant="body2">
+            {user.isMedical ? "Médico" : "Paciente"}
+          </Typography>
+        </Box>
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Button color="primary" fullWidth variant="text">
+          Cambiar foto
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
