bc3c7d57ce45d82bf3cad15493d259cbc43b59d2
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-11-13 14:24:56 -0500

    corrección de errores de Laura

--- src/views/registerGlucosa/index.js
+++ src/views/registerGlucosa/index.js
@@ -5,17 +5,18 @@ import { DashboardLayout } from "../../components/dashboard/dashboard-layout";
 import Loading from "../../components/loading";
 import BasicModal from "../../components/modal";
 import Input from "../../components/input";
-import ButtonComponent from "../../components/button"
+import ButtonComponent from "../../components/button";
 import { ListResults } from "../../components/tableData/list-results";
 import { ListToolbar } from "../../components/tableData/list-toolbar";
 import { customers } from "../../__mocks__/customers";
 
 const RegisterGlucosa = () => {
-  const dataSend = {
-    ...data,
-    date: new Date()
-  }
   const [loading, setLoading] = useState(true);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [data, setData] = useState({});
 
   // const token = window.localStorage.token;
   // if (token) {
@@ -24,30 +25,27 @@ const RegisterGlucosa = () => {
       {/* {loading && <Loading />} */}
       <ButtonComponent text="Agregar registro" onClick={handleOpen} />
 
-        <BasicModal isOpen={open} handleClose={handleClose}>
-                <div className="container-modal">
-                  <h2 className="subtitle">Registro de Glucosa</h2>
+      <BasicModal isOpen={open} handleClose={handleClose}>
+        <div className="container-modal">
+          <h2 className="subtitle">Registro de Glucosa</h2>
 
-                  <Input
-                    type="text"
-                    placeholder="Ejem: 120"
-                    label="Medición Glucosa"
-                    autofocus
-                   
-                  />
+          <Input
+            type="text"
+            placeholder="Ejem: 120"
+            label="Medición Glucosa"
+            autofocus
+          />
 
-                  <Input
-                    type="text"
-                    placeholder="Síntomas relevantes"
-                    label="Mensaje"
-                    autofocus
-                   
-                  />
-                  <br/>
-                  <ButtonComponent text="Guardar" />
-                </div>
-
-              </BasicModal>
+          <Input
+            type="text"
+            placeholder="Síntomas relevantes"
+            label="Mensaje"
+            autofocus
+          />
+          <br />
+          <ButtonComponent text="Guardar" />
+        </div>
+      </BasicModal>
       <Box
         component="main"
         sx={{
