34151f25064c3f2ebb9337a9655387677fd0bb8b
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-11-13 17:00:47 -0500

    Creando vista de reportes

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,17 +1,50 @@
-import React, { useState } from "react";
+import { Box, Container, Grid } from "@mui/material";
+import React from "react";
 import { Navigate } from "react-router-dom";
 import { DashboardLayout } from "../../components/dashboard/dashboard-layout";
-import Loading from "../../components/loading";
+import { Budget } from "../../components/graph/budget";
+import { LatestOrders } from "../../components/graph/latest-orders";
+import { Sales } from "../../components/graph/sales";
+import { TasksProgress } from "../../components/graph/tasks-progress";
+import { TotalCustomers } from "../../components/graph/total-customers";
+import { TotalProfit } from "../../components/graph/total-profit";
+import { TrafficByDevice } from "../../components/graph/traffic-by-device";
 
 const Dashboard = () => {
-  const [loading, setLoading] = useState(true);
-
   const token = window.localStorage.token;
   if (token) {
     return (
       <DashboardLayout>
-        {/* {loading && <Loading />} */}
-        <h1>Estoy en la vista dashboard</h1>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            py: 8,
+          }}
+        >
+          <Container maxWidth={false}>
+            <Grid container spacing={3}>
+              <Grid item lg={3} sm={6} xl={3} xs={12}>
+                <Budget />
+              </Grid>
+              <Grid item xl={3} lg={3} sm={6} xs={12}>
+                <TotalProfit sx={{ height: "100%" }} />
+              </Grid>
+              <Grid item xl={3} lg={3} sm={6} xs={12}>
+                <TotalCustomers />
+              </Grid>
+              <Grid item xl={3} lg={3} sm={6} xs={12}>
+                <TasksProgress />
+              </Grid>
+              <Grid item lg={8} md={12} xl={9} xs={12}>
+                <Sales />
+              </Grid>
+              <Grid item lg={4} md={6} xl={3} xs={12}>
+                <TrafficByDevice sx={{ height: "100%" }} />
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
       </DashboardLayout>
     );
   }
