a86f42ae28b2ed481d837277342bc5e94974d935
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-11-14 20:32:14 -0500

    Agregando el State mas global para usar el contexto en todo el APP

--- src/App.js
+++ src/App.js
@@ -6,48 +6,57 @@ import Dashboard from "./views/dashboard";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme";
 import { CssBaseline } from "@mui/material";
-import UserState from "../src/contexts/user/userState";
 import { ToastContainer } from "react-toastify";
 import RegisterGlucosa from "./views/registerGlucosa";
 import AccountUser from "./views/account";
 import Settings from "./views/settings";
+import ListPatient from "./views/listPatient";
 import RegisterObservacion from "./views/registerObservacion";
 import { registerChartJs } from "./utils/register-chart-js";
 import Page404 from "./views/404";
 import AuthState from "./contexts/auth";
 import { ProtectedRoutes } from "./components/protectedRoutes";
 import { ValidateUser } from "./utils/validate-user";
+import { useContext } from "react";
+import UserContext from "./contexts/user/userContext";
 
 registerChartJs();
 
 function App() {
+
+  const { user } = useContext(UserContext);
+
   return (
     <AuthState>
-      <UserState>
-        <ValidateUser>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <div className="App">
-              <ToastContainer />
-              <Routes>
-                <Route path="*" element={<Page404 />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route element={<ProtectedRoutes />}>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="/glucosa" element={<RegisterGlucosa />} />
-                  <Route path="/cuenta" element={<AccountUser />} />
-                  <Route path="/configuraciones" element={<Settings />} />
-                  <Route
-                    path="/observaciones"
-                    element={<RegisterObservacion />}
-                  />
-                </Route>
-              </Routes>
-            </div>
-          </ThemeProvider>
-        </ValidateUser>
-      </UserState>
+      <ValidateUser>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="App">
+            <ToastContainer />
+            <Routes>
+              <Route path="*" element={<Page404 />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route element={<ProtectedRoutes />}>
+                <Route
+                  path="/"
+                  element={
+                    user?.isMedical ? <ListPatient /> : <Dashboard />
+                  }
+                />
+                <Route path="/glucosa" element={<RegisterGlucosa />} />
+                <Route path="/cuenta" element={<AccountUser />} />
+                <Route path="/configuraciones" element={<Settings />} />
+                {/* <Route path="/pacientes" element={<ListPatient />} /> */}
+                <Route
+                  path="/observaciones"
+                  element={<RegisterObservacion />}
+                />
+              </Route>
+            </Routes>
+          </div>
+        </ThemeProvider>
+      </ValidateUser>
     </AuthState>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './assets/fonts/Inter-Light.ttf';
-import './assets/fonts/Inter-Medium.ttf';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./assets/fonts/Inter-Light.ttf";
+import "./assets/fonts/Inter-Medium.ttf";
+import { BrowserRouter } from "react-router-dom";
+import UserState from "../src/contexts/user/userState";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <UserState>
+      <App />
+    </UserState>
   </BrowserRouter>
 );
 
